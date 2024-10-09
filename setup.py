5276249c3715d11c4508eaa3b771f1d632689947
Mateusz <mateusz.bialek@algolytics.pl>
2024-10-09 16:52:41 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    name='great-module',
+    version='0.1.0',
+    description="Blah.",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/Algolytics/dq_client',
+    packages=[
+        'great_module',
+    ],
+    package_dir={'great_module': 'great_module'},
+    include_package_data=True,
+    test_suite='tests'
+)
